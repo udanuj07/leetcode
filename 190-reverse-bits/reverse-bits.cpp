fe97aa477fdf04ece87bf35fecf59ca0009d837a
udanuj07 <anuj2977013@gmail.com>
2026-02-14 22:46:02 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (93.27%) - LeetSync

--- 190-reverse-bits/reverse-bits.cpp
+++ 190-reverse-bits/reverse-bits.cpp
@@ -1,21 +1,12 @@
 class Solution {
-    uint32_t rev8[256];
 public:
-    Solution() {
-        for (int i = 0; i < 256; ++i) {
-            uint32_t x = i, r = 0;
-            for (int b = 0; b < 8; ++b) {
-                r = (r << 1) | (x & 1);
-                x >>= 1;
-            }
-            rev8[i] = r;
-        }
-    }
-
     uint32_t reverseBits(uint32_t n) {
-        return (rev8[n & 0xff] << 24) |
-               (rev8[(n >> 8) & 0xff] << 16) |
-               (rev8[(n >> 16) & 0xff] << 8) |
-               (rev8[(n >> 24) & 0xff]);
+        uint32_t ans = 0;
+        for (int i = 0; i < 32; ++i) {
+            ans <<= 1;
+            ans |= (n & 1);
+            n >>= 1;
+        }
+        return ans;
     }
 };
