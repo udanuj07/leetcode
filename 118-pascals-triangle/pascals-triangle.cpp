5d2729de14e6b449187226e64e6a759d07920d39
udanuj07 <anuj2977013@gmail.com>
2026-02-25 20:26:44 +0530

    Time: 1 ms (18.79%) | Memory: 9.7 MB (30.64%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> res(numRows);
+        for (int i = 0; i < numRows; ++i) {
+            res[i].assign(i + 1, 1);
+            for (int j = 1; j < i; ++j) res[i][j] = res[i - 1][j - 1] + res[i - 1][j];
+        }
+        return res;
+    }
+};
