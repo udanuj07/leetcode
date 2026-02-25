019552609d4d4d10277912a09a78274be29b87db
udanuj07 <anuj2977013@gmail.com>
2026-02-25 20:15:43 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (67.79%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,31 @@
+#include <queue>
+using namespace std;
+
+class MyStack {
+    queue<int> q;
+public:
+    MyStack() {}
+    
+    void push(int x) {
+        q.push(x);
+        int k = q.size() - 1;
+        while (k--) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
