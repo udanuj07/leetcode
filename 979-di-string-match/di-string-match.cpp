062dfc2021abd1632d24bb3857178783c25e2d78
udanuj07 <anuj2977013@gmail.com>
2026-03-08 20:50:19 +0530

    Time: 0 ms (100.00%) | Memory: 11.4 MB (90.34%) - LeetSync

---
+++ 979-di-string-match/di-string-match.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> diStringMatch(string s) {
+        int n = s.size();
+        vector<int> ans(n + 1);
+        int low = 0, high = n;
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == 'I') ans[i] = low++;
+            else ans[i] = high--;
+        }
+        ans[n] = low;
+        return ans;
+    }
+};
