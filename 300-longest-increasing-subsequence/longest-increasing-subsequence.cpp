9f422633a2759ddd6ab6741218fd3cfff5925405
udanuj07 <anuj2977013@gmail.com>
2026-09-19 13:15:52 +0530

    Time: 452 ms (26.11%) | Memory: 41.7 MB (34.67%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,12 +1,31 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int> tails;
-        for (int x : nums) {
-            auto it = lower_bound(tails.begin(), tails.end(), x);
-            if (it == tails.end()) tails.push_back(x);
-            else *it = x;
+    int dp[2501][2503];
+
+    int fun(int i, vector<int>& nums, int prev) {
+
+        if (i >= nums.size())
+            return 0;
+
+        if (dp[i][prev + 1] != -1)
+            return dp[i][prev + 1];
+
+        int m = 0;
+
+        if (prev == -1 || nums[i] > nums[prev]) {
+            int a = 1 + fun(i + 1, nums, i);
+            m = max(m, a);
         }
-        return tails.size();
+
+        int b = fun(i + 1, nums, prev);
+
+        return dp[i][prev + 1] = max(m, b);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+
+        memset(dp, -1, sizeof(dp));
+
+        return fun(0, nums, -1);
     }
 };
